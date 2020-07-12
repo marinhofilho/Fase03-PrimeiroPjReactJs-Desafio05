242296d72f5f14cf525ea425f196dba6579dabb5
marinhofilho <mariolemanf@gmail.com>
2020-07-12 19:55:24 -0300

    First Commit Desafio05

---
+++ build/precache-manifest.2115bcebf825474766a32b7952c528c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4868b03148ed8d15f6c847d05605a14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7f7b3e1d1bb768add9a",
+    "url": "/static/js/2.93c5725d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.93c5725d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3d600ac4246a4403372",
+    "url": "/static/js/main.4ff8b700.chunk.js"
+  },
+  {
+    "revision": "fe5aa25235cdbf3247f5",
+    "url": "/static/js/runtime-main.e6d31b63.js"
+  }
+]);
